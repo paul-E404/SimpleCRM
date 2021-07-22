d6775d370706348e9915f625137fa319d6fa5bad
paul-E404 <paul_engerling@web.de>
2021-07-22 13:12:59 +0200

    Started with responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
   title = 'simple-crm';
 
-  menuOpened: boolean = false;
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit() {
-  
+
+  }
+
+  //source: https://zoaibkhan.com/blog/create-a-responsive-sidebar-menu-with-angular-material/
+  ngAfterViewInit() {
+    
+    setTimeout(() => {
+      this.observer.observe(['(max-width: 1000px)']).subscribe((result) => {
+        if (result.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+    }, 0);
+
   }
 }
